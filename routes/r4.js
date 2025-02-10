a1feed8430c828af4c1a72e8da1696c1343c7fb9
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-02-09 19:14:29 -0500

    Add files via upload

--- routes/r4.js
+++ routes/r4.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
-const path = require("path");
 
 const router = Router();
 
-const Admission = require(path.join(__dirname, "../data", "./admission.js"));
+const Admission = [{}];
 
 router.get("/pages/r4", (req, res) => {
   res.json(Admission);
