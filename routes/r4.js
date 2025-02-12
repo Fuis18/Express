c486f424c5c4bde12f92949ccb2c38d6f8affe62
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-02-12 04:40:23 -0500

    2

--- routes/r4.js
+++ routes/r4.js
@@ -3,18 +3,17 @@ const { Router } = require("express");
 
 const router = Router();
 
-// const Admission = require(path.join(__dirname, "../data", "./admission.js"));
-const Admission = [];
+const Admission = require(path.join(__dirname, "../data", "./admission.js"));
 
 router.get("/pages/r4", (req, res) => {
   res.json(Admission);
 });
 
-// router.post("/pages/r4", (req, res) => {
-//   console.log("Datos recibidos:", req.body);
-//   const item = { ...req.body, id: Admission.length + 1 };
-//   Admission.push(item);
-//   res.json({ message: Admission });
-// });
+router.post("/pages/r4", (req, res) => {
+  console.log("Datos recibidos:", req.body);
+  const item = { ...req.body, id: Admission.length + 1 };
+  Admission.push(item);
+  res.json({ message: Admission });
+});
 
 module.exports = router;
