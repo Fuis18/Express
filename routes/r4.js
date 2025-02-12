ba0f53b9d4a210eec7b868d569cb4e0e26a49dfc
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-02-12 04:38:50 -0500

    vamos por más

--- routes/r4.js
+++ routes/r4.js
@@ -10,11 +10,11 @@ router.get("/pages/r4", (req, res) => {
   res.json(Admission);
 });
 
-router.post("/pages/r4", (req, res) => {
-  console.log("Datos recibidos:", req.body);
-  const item = { ...req.body, id: Admission.length + 1 };
-  Admission.push(item);
-  res.json({ message: Admission });
-});
+// router.post("/pages/r4", (req, res) => {
+//   console.log("Datos recibidos:", req.body);
+//   const item = { ...req.body, id: Admission.length + 1 };
+//   Admission.push(item);
+//   res.json({ message: Admission });
+// });
 
 module.exports = router;
