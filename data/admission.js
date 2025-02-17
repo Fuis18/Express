6ac61325c94d46de9305c781a98c828d77010735
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-02-16 23:27:16 -0500

    Add files via upload

---
+++ data/admission.js
@@ -0,0 +1,3 @@
+const Admission = [];
+
+module.exports = Admission;
--- routes/r4.js
+++ routes/r4.js
@@ -10,11 +10,22 @@ router.get("/pages/r4", (req, res) => {
   res.json(Admission);
 });
 
+// Agregar un nuevo registro
 router.post("/pages/r4", (req, res) => {
   console.log("Datos recibidos:", req.body);
-  const item = { ...req.body, id: Admission.length + 1 };
-  Admission.push(item);
+  const newItem = { ...req.body, id: Admission.length + 1 };
+  Admission.push(newItem);
   res.json({ message: Admission });
+});
+
+
+// Enviar la lista por correo y vaciar Admission
+router.post("/pages/r4/mail", async (req, res) => {
+  if (Admission.length === 0) {
+    console.log("Correo enviado con éxito");
+    Admission = []; // Limpiar la lista después de enviar
+    return res.status(400).json({ error: "No hay datos para enviar" });
+  }
 });
 
 module.exports = router;
